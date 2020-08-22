a2756185e18967b649bf515878f87eaa8adb7398
Rex <78131306@qq.com>
2020-08-22 10:13:19 +0800

    v1.0

--- gittest/src/com/py/GitTest.java
+++ gittest/src/com/py/GitTest.java
@@ -2,6 +2,6 @@ package com.py;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("helloworld!！");
+        System.out.println("helloworld！");
     }
 }
